fc36e6bf9db0f9e3e3224135c88d59240b2fa13b
Filip Kolesár <filipkolesar@Filips-MacBook-Pro.local>
2018-10-04 22:23:44 +0200

    implement text replace as option

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ export const setTranslations = (translations: {[locale: string]: Object}) => {
 	appTranslations = translations;
 };
 
-export const getTranslatedText = (key: string) => {
+export const getTranslatedText = (translationKey: string, options: Object) => {
 	const actualTranslations = appTranslations[appLanguage];
 	if (!actualTranslations) return NO_TRANSLATION_FILES;
-	return actualTranslations[key] || `${appLanguage}_${key}`;
+	if (!actualTranslations[translationKey]) return `${appLanguage}_${translationKey}`;
+	if (options) {
+		let translation = actualTranslations[translationKey];
+		Object.keys(options).map((key) => {
+			translation = translation.replace(`{{${key}}}`, options[key]);
+		});
+		return translation;
+	} else {
+		return actualTranslations[translationKey];
+	}
 };
 
 export const setAppLanguage = (language: string): Promise<any> => {
